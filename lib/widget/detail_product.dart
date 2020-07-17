87234a5e9896d5c398b0099e4e20a8c8c0dd9da8
piyawat <piyawat@gmail.com>
2020-07-17 15:15:33 +0700

    finish 17Jul

--- lib/widget/detail_product.dart
+++ lib/widget/detail_product.dart
@@ -28,24 +28,42 @@ class _DetailProductState extends State<DetailProduct> {
         children: <Widget>[
           buildText(context, 'รหัสสินค้า = ${model.code}'),
           buildText(context, 'ชื่อสินค้า = ${model.name}'),
-        ],
+          learnXd()
+                  ],
+                ),
+              );
+            }
+          
+            Widget buildText(BuildContext context, String string) {
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Card(
+                            child: Container(padding: EdgeInsets.all(8),
+                        width: MediaQuery.of(context).size.width * 0.8,
+                        child: Text(
+                          string,
+                          style: MyStyle().titleH2(),
+                        )),
+                  ),
+                ],
+              );
+            }
+          
+            Widget learnXd() {
+              return Container(
+    width: 287.0,
+    height: 64.0,
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(25.0),
+      gradient: LinearGradient(
+        begin: Alignment(0.0, -0.84),
+        end: Alignment(0.0, 1.0),
+        colors: [const Color(0xedf97a10), const Color(0xff7b3b03)],
+        stops: [0.0, 1.0],
       ),
-    );
-  }
-
-  Widget buildText(BuildContext context, String string) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Card(
-                  child: Container(padding: EdgeInsets.all(8),
-              width: MediaQuery.of(context).size.width * 0.8,
-              child: Text(
-                string,
-                style: MyStyle().titleH2(),
-              )),
-        ),
-      ],
-    );
-  }
+      border: Border.all(width: 1.0, color: const Color(0xff707070)),
+    ),
+  );
+            }
 }
