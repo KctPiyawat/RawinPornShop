75d8d818f23da7bc25c1f1957429646f48f8ece6
piyawat <piyawat@gmail.com>
2020-07-17 14:25:49 +0700

    update

--- lib/widget/detail_product.dart
+++ lib/widget/detail_product.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:rawinpornshop/models/search_model.dart';
+import 'package:rawinpornshop/utility/my_style.dart';
 
 class DetailProduct extends StatefulWidget {
   final SearchModel searchModel;
@@ -10,7 +11,6 @@ class DetailProduct extends StatefulWidget {
 }
 
 class _DetailProductState extends State<DetailProduct> {
-
   SearchModel model;
 
   @override
@@ -19,10 +19,33 @@ class _DetailProductState extends State<DetailProduct> {
     super.initState();
     model = widget.searchModel;
   }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(),
-      body: Text('name = ${model.name}'),
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: <Widget>[
+          buildText(context, 'รหัสสินค้า = ${model.code}'),
+          buildText(context, 'ชื่อสินค้า = ${model.name}'),
+        ],
+      ),
+    );
+  }
+
+  Widget buildText(BuildContext context, String string) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Card(
+                  child: Container(padding: EdgeInsets.all(8),
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: Text(
+                string,
+                style: MyStyle().titleH2(),
+              )),
+        ),
+      ],
     );
   }
 }
