b23d07dfe9c4cbd5199fa8265a83941dabace278
piyawat <piyawat@gmail.com>
2020-10-30 15:27:36 +0700

    finish 30Oct 2020

--- lib/utility/my_constant.dart
+++ lib/utility/my_constant.dart
@@ -1,14 +1,19 @@
 class MyConstant {
-
-
   String domain = 'http://210.86.171.110:89';
   String whidLatkabang = '7';
   String whidEuamp = '8';
   String whidNongjok = '9';
   String whidHunjakai = '10';
 
+  List<String> menuFrontEnds = ['Search', 'Menu2', 'Menu3', 'Menu4', 'Menu5'];
+  List<String> imageMenuFrontEnds = [
+    'icon0',
+    'icon1',
+    'icon2',
+    'icon3',
+    'icon4',
+    'icon5'
+  ];
 
   MyConstant();
-
-  
 }
---
+++ lib/widget/backend_menu.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class BackEndMenu extends StatefulWidget {
+  @override
+  _BackEndMenuState createState() => _BackEndMenuState();
+}
+
+class _BackEndMenuState extends State<BackEndMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('This is BackEndMenu'),
+      
+    );
+  }
+}
